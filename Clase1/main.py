489a507eba9e39bbdcd4ad1f0690519ac4106b96
Di3go004 <3590681460101@ingenieria.usac.edu.gt>
2024-07-27 16:44:36 -0600

    prueba1

---
+++ Clase1/main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    print("Hola esty en el main")  
+    # este es un comentario de una linea
+    '''
+    Este 
+    es 
+    un 
+    comentario 
+    multilinea
+    '''
+
