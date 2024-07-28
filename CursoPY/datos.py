33a7cbaff7c013f07f205ea6bdf65dffe450d2dd
Di3go004 <3590681460101@ingenieria.usac.edu.gt>
2024-07-27 22:12:32 -0600

    2do commit

---
+++ CursoPY/datos.py
@@ -0,0 +1,50 @@
+name = "Diego Alejandro"
+last_name ="Gonzalez Morales"
+print(type(name)) # <class 'str'>
+print(name[3]) # g
+print(name[0:5]) # Diego
+print(name[-2]) # r
+print(name[0:5:2]) # Deo
+print(name + " " + last_name) # Diego Alejandro Gonzalez Morales
+print(name * 3) # Diego AlejandroDiego AlejandroDiego Alejandro
+print(len(last_name)) # 16
+
+#Tipos de datos 
+
+x = 10
+y = 10.145
+z = 1.2e6
+a = 4.3e-3
+print (type(x)) # <class 'int'>
+print (type(y)) # <class 'float'>
+print (z) # 1200000.0
+print (a) # 0.0043
+print (x + y) # 20.145
+print (x + x**2) # 110
+
+#Boolean
+
+is_true = True
+is_false = False
+print(type(is_true)) # <class 'bool'>
+
+
+#Operadores numericos
+
+a = 10
+b = 3
+print(a + b) # 13
+print(a - b) # 7
+print(a * b) # 30
+print(a / b) # 3.3333333333333335
+print(a // b) # 3 (division entera)
+print(a % b) # 1 (modulo)
+print(a ** b) # 1000 (potencia)
+print(a > b) # True
+print(a < b) # False
+print(a = b ) # False
+print(a == b) # False
+print(a != b) # True
+
+
+
---
+++ CursoPY/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+name = "Diego"
+print(type(name))
